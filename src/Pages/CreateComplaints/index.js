492eb5fbada3e1ccbb0f9c2ac37c75b2f8380c4e
Juan Carlos Espinoza <jc_espinozarodriguez@hotmail.com>
2024-11-21 23:44:21 -0600

    Se añadio el debido archivo para CreateComplaints

---
+++ src/Pages/CreateComplaints/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { CustomText } from '../../Components/Text'
+
+function CreateComplaints() {
+    return (
+        <CustomText>
+            Hola aqui se crean las quejas
+        </CustomText>
+    )
+}
+
+export { CreateComplaints } 
