be6ea020500209fc61a860fa82d1cf5a75edc8fc
Juan Carlos Espinoza <jc_espinozarodriguez@hotmail.com>
2024-11-21 23:18:08 -0600

    Se mejoro la logica de envolvimiento de los componentes en index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import { ChangeUserView } from './Services/ChangeUserView';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Router>
-        <App />
+        <ChangeUserView>
+            <App />
+        </ChangeUserView>
     </Router>
 );
