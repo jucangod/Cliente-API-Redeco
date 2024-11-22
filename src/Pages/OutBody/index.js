ce6ee4b90140056484a7fb1bbb67d9b2e5eea991
Juan Carlos Espinoza <jc_espinozarodriguez@hotmail.com>
2024-11-22 00:06:32 -0600

    Coreccion de errores dentro de LoggedBody y OutBody

--- src/Pages/OutBody/index.js
+++ src/Pages/OutBody/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AppContext } from '../../Services/ChangeUserView';
-import { SignUp } from './Pages/SignUp';
-import { Login } from './Pages/Login';
+import { SignUp } from '../SignUp';
+import { Login } from '../Login';
 
 function OutBody() {
     const {
