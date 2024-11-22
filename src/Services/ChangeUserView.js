fd836665a5b29ffdd5ad1f9e6c52c554885f4532
Juan Carlos Espinoza <jc_espinozarodriguez@hotmail.com>
2024-11-21 23:20:31 -0600

    Implementacion de la logica para determinar si un usuario esta logeado en ChangeUserView

--- src/Services/ChangeUserView.js
+++ src/Services/ChangeUserView.js
@@ -4,13 +4,23 @@ const AppContext = React.createContext();
 
 function ChangeUserView({ children }) {
     const [loginPage, setLoginPage] = React.useState(true);
+    const [isUserLogged, setIsUserLogged] = React.useState(false);
 
     const changeView = () => {
         setLoginPage(!loginPage);
     }
 
+    const logUser = () => {
+        setIsUserLogged(!isUserLogged)
+    }
+
     return (
-        <AppContext.Provider value={{loginPage, changeView}}>
+        <AppContext.Provider value={{
+            loginPage, 
+            changeView,
+            isUserLogged,
+            logUser
+        }}>
             {children}
         </AppContext.Provider>
     );
