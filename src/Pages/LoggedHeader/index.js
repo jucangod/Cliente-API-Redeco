fbb9bf6db6662254824ab945f1203a3967511954
Juan Carlos Espinoza <jc_espinozarodriguez@hotmail.com>
2024-11-21 23:43:12 -0600

    El commit anterior se hizo sin hacer un add, por si las dudas lo aclaro en este

---
+++ src/Pages/LoggedHeader/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { CustomButton } from '../../Components/Button'
+import { AppContext } from './Services/ChangeUserView';
+
+function LoggedHeader() {
+    const {
+        logUser,
+        changeComplaints
+    } = React.useContext(AppContext);
+
+    return (
+        <>
+            <CustomButton
+                onClick={() => changeComplaints('ver')}
+            >
+                Ver
+            </CustomButton>
+            <CustomButton
+                onClick={() => changeComplaints('crear')}
+            >
+                Crear
+            </CustomButton>
+            <CustomButton
+                onClick={logUser}
+            >
+                Cerrar Sesión
+            </CustomButton>
+        </>
+    )
+}
+
+export { LoggedHeader}
---
+++ src/Pages/SeeComplaints/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { CustomText } from '../../Components/Text'
+
+function SeeComplaints() {
+    return (
+        <CustomText>
+            Hola aqui estan las quejas
+        </CustomText>
+    )
+}
+
+export { SeeComplaints } 
