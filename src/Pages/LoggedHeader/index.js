b1bfc91b4f8311b9efe95dcd994ab6409782b2db
Bbenrod <benjamintec3455@gmail.com>
2024-12-15 23:05:51 -0600

    Manejo de token

--- src/Pages/LoggedHeader/index.js
+++ src/Pages/LoggedHeader/index.js
@@ -1,46 +1,41 @@
-import React from 'react'
+import React from 'react';
 import { CustomButton } from '../../Components/Button';
 import { AppContext } from '../../Services/ChangeUserView';
 import { CustomModal } from '../../Components/Modal';
-import './LoggedHeader.css'
+import './LoggedHeader.css';
 
 function LoggedHeader() {
-    const {
-        logUser,
-        changeComplaints,
-        handleClose,
-        isModalOpen,
-        cancelClose
-    } = React.useContext(AppContext);
+  const { logUser, changeComplaints, handleClose, isModalOpen, cancelClose } =
+    React.useContext(AppContext);
 
-    return (
-        <div id='header-section'>
-            <CustomButton 
-                className='complaint-button'
-                onClick={() => changeComplaints('ver')}
-            >
-                Ver
-            </CustomButton>
-            <CustomButton
-                className='complaint-button'
-                onClick={() => changeComplaints('crear')}
-            >
-                Crear
-            </CustomButton>
-            <CustomButton
-                className='complaint-button'
-                onClick={handleClose}
-            >
-                Cerrar Sesión
-            </CustomButton>
-            <CustomModal
-                isOpen={isModalOpen}
-                message="¿Estás seguro de que deseas cerrar sesión?"
-                onConfirm={logUser}
-                onCancel={cancelClose}
-            />
-        </div>
-    )
+  return (
+    <div id="header-section">
+      <CustomButton
+        className="complaint-button"
+        onClick={() => changeComplaints('ver')}
+      >
+        Ver
+      </CustomButton>
+      <CustomButton
+        className="complaint-button"
+        onClick={() => changeComplaints('crear')}
+      >
+        Crear
+      </CustomButton>
+      <CustomButton className="complaint-button" onClick={handleClose}>
+        Cerrar Sesión
+      </CustomButton>
+      <CustomModal
+        isOpen={isModalOpen}
+        message="¿Estás seguro de que deseas cerrar sesión?"
+        onConfirm={() => {
+          logUser();
+          localStorage.removeItem('token_access');
+        }}
+        onCancel={cancelClose}
+      />
+    </div>
+  );
 }
 
-export { LoggedHeader}
+export { LoggedHeader };
--- src/Services/ChangeUserView.js
+++ src/Services/ChangeUserView.js
@@ -1,53 +1,55 @@
-import React from 'react'
+import React from 'react';
 
 const AppContext = React.createContext();
 
 function ChangeUserView({ children }) {
-    const [loginPage, setLoginPage] = React.useState(true);
-    const [isUserLogged, setIsUserLogged] = React.useState(false);
-    const [loggedUserView, setLoggedUserView] = React.useState('ver');
-    const [isModalOpen, setModalOpen] = React.useState(false);
+  const [loginPage, setLoginPage] = React.useState(true);
+  const [isUserLogged, setIsUserLogged] = React.useState(false);
+  const [loggedUserView, setLoggedUserView] = React.useState('ver');
+  const [isModalOpen, setModalOpen] = React.useState(false);
 
-    const handleClose = () => {
-        setModalOpen(true)
-    }
+  const handleClose = () => {
+    setModalOpen(true);
+  };
 
-    const cancelClose = () => {
-        setModalOpen(false)
-    }
+  const cancelClose = () => {
+    setModalOpen(false);
+  };
 
-    const changeView = () => {
-        setLoginPage(!loginPage);
-    }
+  const changeView = () => {
+    setLoginPage(!loginPage);
+  };
 
-    const logUser = () => {
-        setIsUserLogged(!isUserLogged)
-        setModalOpen(false)
-        setLoginPage(true)
-        if (loginPage) {
-            localStorage.removeItem('token_access');
-        }
-    }
+  const logUser = () => {
+    setIsUserLogged(!isUserLogged);
+    setModalOpen(false);
+    setLoginPage(true);
+    // if (loginPage) {
+    //     localStorage.removeItem('token_access');
+    // }
+  };
 
-    const changeComplaints = (view) => {
-        setLoggedUserView(view)
-    }
+  const changeComplaints = (view) => {
+    setLoggedUserView(view);
+  };
 
-    return (
-        <AppContext.Provider value={{
-            loginPage, 
-            changeView,
-            isUserLogged,
-            logUser,
-            loggedUserView,
-            changeComplaints,
-            handleClose,
-            isModalOpen,
-            cancelClose
-        }}>
-            {children}
-        </AppContext.Provider>
-    );
-};
+  return (
+    <AppContext.Provider
+      value={{
+        loginPage,
+        changeView,
+        isUserLogged,
+        logUser,
+        loggedUserView,
+        changeComplaints,
+        handleClose,
+        isModalOpen,
+        cancelClose,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+}
 
 export { ChangeUserView, AppContext };
